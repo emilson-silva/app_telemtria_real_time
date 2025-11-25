0d4c1831b7c0285b4585cad4a1ed65a805b61bda
Emilson Silva <emilsongyn17@gmail.com>
2025-11-25 13:04:25 -0300

    feat(domain): implement clean architecture domain layer
    Create domain layer following clean architecture principles:

--- lib/domain/entities/telemetry_data.dart
+++ lib/domain/entities/telemetry_data.dart
@@ -1,1 +1,39 @@
-class TelemetryData {}
+class TelemetryData {
+  final double latitude;
+  final double longitude;
+  final double altitude;
+  final double speed;
+  final double accuracy;
+  final double heading;
+
+  final double accelerometerX;
+  final double accelerometerY;
+  final double accelerometerZ;
+
+  final double gyroscopeX;
+  final double gyroscopeY;
+  final double gyroscopeZ;
+  final double magnetometerX;
+  final double magnetometerY;
+  final double magnetometerZ;
+
+  final DateTime timestamp;
+  const TelemetryData({
+    required this.latitude,
+    required this.longitude,
+    required this.altitude,
+    required this.speed,
+    required this.accuracy,
+    required this.heading,
+    required this.accelerometerX,
+    required this.accelerometerY,
+    required this.accelerometerZ,
+    required this.gyroscopeX,
+    required this.gyroscopeY,
+    required this.gyroscopeZ,
+    required this.magnetometerX,
+    required this.magnetometerY,
+    required this.magnetometerZ,
+    required this.timestamp,
+  });
+}
